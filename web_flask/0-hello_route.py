75b71ecd247e9a5fab1709bcb987042077510d22
Adib-96 <adibbh1996@gmail.com>
2023-12-14 21:15:23 +0100

    pycode style fix

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
+"""
+Flask application
+"""
+
 from flask import Flask
 
 
 app = Flask(__name__)
 
-@app.route('/',strict_slashes=False)
+
+@app.route('/', strict_slashes=False)
 def hello_world():
+    """Display a message to the user"""
     return "<p>Hello HBNB!</p>"
 
 
 if __name__ == '__main__':
+    """Run the main program"""
     app.run()
